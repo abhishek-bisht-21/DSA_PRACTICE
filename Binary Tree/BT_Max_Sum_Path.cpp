9c2ed6046402b6c9c98ce7d9bc1ecdfdf0c3d9a0
Abhishek <abhishek.bisht54@yahoo.com>
2022-01-31 23:12:19 +0530

    Bt Max Sum Path btw any two nodes

--- Binary Tree/BT_Max_Sum_Path.cpp
+++ Binary Tree/BT_Max_Sum_Path.cpp
@@ -1,2 +1,20 @@
 // 124. Binary Tree Maximum Path Sum
 
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        maxPathSumDown(root,maxi);
+        return maxi;
+    }
+    
+    int maxPathSumDown(TreeNode* root, int &maxi){
+        if(root == nullptr) return 0;
+        
+        int lsum = max(0,maxPathSumDown(root->left,maxi));
+        int rsum = max(0,maxPathSumDown(root->right,maxi));
+        maxi = max(maxi,lsum+rsum+root->val);
+        return max(lsum,rsum) + root->val;
+        
+    }
+};
