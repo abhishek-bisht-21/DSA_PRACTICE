3ea52a433688cef29995d6d4cb73d1050c63093c
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-10 01:01:06 +0530

    Delete nth Node from End

---
+++ LinkedList/7.delete_nth_node_frmEnd.cpp
@@ -0,0 +1,78 @@
+// 19. Remove Nth Node From End of List
+
+// Approach 1
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+         //Counting the number of nodes in the linked list.       
+      ListNode *temp=head;
+      int countNodes=0;
+      while(temp!=NULL)
+      {
+          countNodes++;
+          temp=temp->next;
+      }
+        
+      //Checking the edge case when the 'n' i.e. node to be deleted
+      //from end of the list is equal to the number of nodes in the 
+      //linked list.
+      /*For Example :- 
+                       Input : [7,4] , n=2
+                       Output: [4]
+      */
+      if(countNodes==n)
+      {
+          ListNode *node=head;
+          head=head->next;
+          delete node;
+          return head;
+      }
+        
+      //If the node to be deleted is not the head of the list then we have 
+      //to reach one node before it.         
+      int difference = countNodes - n;
+      int counter=1;
+      temp=head;
+      while(counter!=difference)
+      {
+          counter++;
+          temp=temp->next;
+      }
+        
+      //The main logic to delete the nth node from the end.       
+      ListNode *node=temp->next;
+      temp->next=node->next;
+      delete node;
+        
+      return head;
+    }
+};
+
+
+// Approach 2
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode;
+        dummy -> next = head;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        
+        
+        for(int i=1;i<=n;i++){
+            fast = fast->next;
+        }
+        
+        while(fast->next != nullptr){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        return dummy->next;
+        
+        
+    }
+};
