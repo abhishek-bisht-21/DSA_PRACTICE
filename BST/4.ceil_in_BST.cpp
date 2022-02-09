250a2a561bffb189fd35a4d5fec58f9239768520
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-10 02:59:20 +0530

    Ceil in the BST

---
+++ BST/4.ceil_in_BST.cpp
@@ -0,0 +1,22 @@
+// smallest value who is greater than or equal to the key.
+
+int findCeil(TreeNode* root, int key){
+	int ceil = -1;
+
+	while(root){
+		
+		if(root->data == key){
+			ceil = root->data;
+			return ceil
+		}
+		
+		if(key > root->data){
+			root = root->right;
+		}else{
+			ceil = root->data;
+			root = root->left;
+		}
+	}
+
+	return ceil;
+}
