985ba952719296aa4d86718b5213be3dc0f96a35
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-11 00:05:58 +0530

    LCA in BST

---
+++ BST/10.LCA_in_BST.cpp
@@ -0,0 +1,21 @@
+
+
+// 235. Lowest Common Ancestor of a Binary Search Tree
+
+// Approach 1 (Node from where we cant decide whether we have to go on the right or left)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p -> val < cur -> val && q -> val < cur -> val) {
+                cur = cur -> left;
+            } else if (p -> val > cur -> val && q -> val > cur -> val) {
+                cur = cur -> right;
+            } else {
+                break;
+            }
+        }
+        return cur;
+    }
+};
