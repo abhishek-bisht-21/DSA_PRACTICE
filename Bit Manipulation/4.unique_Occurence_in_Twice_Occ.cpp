25df4acb34ae8999e471c8adaa512548a3565beb
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-17 19:20:04 +0530

    Find element that appears once while all other elements appear twice

---
+++ Bit Manipulation/4.unique_Occurence_in_Twice_Occ.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans = 0;
+        
+        for(int ele: nums){
+            ans ^= ele;
+        }
+        
+        return ans;
+    }
+};
