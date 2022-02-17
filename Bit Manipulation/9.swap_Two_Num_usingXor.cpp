2d48682cf469c8c751327ecda66377b6a187794e
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-17 22:13:48 +0530

    Swap 2 Numbers using Xor

---
+++ Bit Manipulation/9.swap_Two_Num_usingXor.cpp
@@ -0,0 +1,14 @@
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        //complete the function here
+        
+         a = a^b;
+         b = a^b;
+         a = a^b;
+        
+        pair<int,int> p = make_pair(a,b);
+        return p;
+        
+    }
+};
