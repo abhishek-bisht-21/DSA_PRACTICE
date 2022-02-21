59372febbf99257032f33beb6abce201442bf6e2
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-21 06:11:02 +0530

    minimum in rotated sorted array

--- Binary Search/9.minRotatedSortedarr.cpp
+++ Binary Search/9.minRotatedSortedarr.cpp
@@ -6,12 +6,14 @@ public:
         int low = 0;
         int high = n-1;
         
+        // When only 1 element is left in our array then it is surely the minimum element.
         while(low < high){
             int mid = low + (high-low)/2;
-            
+            // We cannot eleminate the mid cause we haven't checked it and it can also be the minimum ele
             if(nums[mid] < nums[high]){
                 high = mid;
             }else{
+                // We are eleminating the entire left part cause ele their are obviously bigger.
                 low = mid+1;
             }
         }
