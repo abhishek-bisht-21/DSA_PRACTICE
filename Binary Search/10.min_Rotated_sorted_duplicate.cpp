955ab3e6026af75150c1ee6f7422fb27f742a61a
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-21 06:15:02 +0530

    minimum element in rotated sorted array with duplicates

--- Binary Search/10.min_Rotated_sorted_duplicate.cpp
+++ Binary Search/10.min_Rotated_sorted_duplicate.cpp
@@ -9,12 +9,17 @@ public:
         
         while(si < ei){
             int mid = si + (ei-si)/2;
-            
+            // Bring ei at the mid cause we haven't checked mid yet and it can also be the Minimum element.
             if(arr[mid] < arr[ei]){
                 ei = mid;
-            }else if(arr[mid] > arr[ei]){
+            }
+            // Remove the entire lpart of array. Cause element from si to mid are obviously bigger.
+            else if(arr[mid] > arr[ei]){
                 si = mid+1;
-            }else{
+            }
+            // When the mid is equal to ei then we can't straight up eleminate the rpart of array.
+            // Instead we will eleminate that specific element only.
+            else{
                 ei--;
             }
         }
