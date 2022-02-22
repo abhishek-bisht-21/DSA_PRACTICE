59ec39501b9cde290781eeaf01cf1e8531ebd5eb
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-22 12:02:27 +0530

    278. First Bad Version

---
+++ Binary Search/19.leetcode_278.cpp
@@ -0,0 +1,21 @@
+
+// 278. First Bad Version
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        int si = 0;
+        int ei = n;
+        
+        while(si < ei){
+            int mid = si+(ei-si)/2;
+            if(isBadVersion(mid)){
+                ei = mid;
+            }else{
+                si = mid+1;
+            }
+        }
+        
+        return si;
+    }
+};
