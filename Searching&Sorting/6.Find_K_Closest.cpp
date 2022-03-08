0708ee1cb4f99a5bd8c7e80c8781a9bb997aaad8
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-08 08:29:37 +0530

    Find k Closest in Sorted Array

--- Searching&Sorting/6.Find_K_Closest.cpp
+++ Searching&Sorting/6.Find_K_Closest.cpp
@@ -1,3 +1,5 @@
+
+// Approach 1
 class Solution {
 public:
     
@@ -25,3 +27,17 @@ public:
         return ans;
     }
 };
+
+// Approach 2
+
+   vector<int> findClosestElements(vector<int>& A, int k, int x) {
+        int left = 0, right = A.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return vector<int>(A.begin() + left, A.begin() + left + k);
+    }
