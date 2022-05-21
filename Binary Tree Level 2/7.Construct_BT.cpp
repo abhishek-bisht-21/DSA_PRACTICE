936b04e0d871f79d87361a7d6919b175dc75fb6d
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-21 17:38:01 +0530

    Construct BT from Pre and In/ post and in

--- Binary Tree Level 2/7.Construct_BT.cpp
+++ Binary Tree Level 2/7.Construct_BT.cpp
@@ -29,3 +29,40 @@ public:
         return construct(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1);
     }
 };
+
+
+
+// 106. Construct Binary Tree from Inorder and Postorder Traversal
+
+class Solution {
+public:
+    
+     TreeNode* construct(vector<int> post, int psi, int pei, vector<int> in,int isi, int iei){
+        
+        if(psi > pei){
+            return nullptr;
+        }
+        
+        TreeNode* root = new TreeNode(post[pei]);
+        int find_ele = post[pei];
+        
+        int idx = isi;
+        
+        while(in[idx] != find_ele){
+            idx++;
+        }
+        
+        int left_ele = idx - isi;
+        
+        root->left = construct(post,psi,psi+left_ele-1,in,isi,idx-1);
+        root->right = construct(post,psi+left_ele,pei-1,in,idx+1,iei);
+        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        
+         return construct(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1);
+        
+    }
+};
