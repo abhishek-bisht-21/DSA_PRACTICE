1d9d784f944a2e460d6abbc03e0a0d7a93c1c952
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-23 02:22:41 +0530

    Exactly 1 child in BT

---
+++ Binary Tree Level 2/13.ExactlyOneChild.cpp
@@ -0,0 +1,21 @@
+// List Of All Single Child Node In Binary Tree 
+
+void exactlyOneChild_(TreeNode* root, vector<int> ans){
+	if(root == nullptr or (root->left == nullptr and root->right == nullptr)){
+		return;
+	}
+
+	if(root->left == NULL or root->right == NULL){
+		ans.push_back(root->data);
+		return;
+	}
+
+	exactlyOneChild_(root->left,ans);
+	exactlyOneChild_(root->right,ans);
+}
+
+vector<int> exactlyOneChild(TreeNode* root){
+	vector<int> ans;
+	exactlyOneChild_(root,ans);
+	return ans;
+}
