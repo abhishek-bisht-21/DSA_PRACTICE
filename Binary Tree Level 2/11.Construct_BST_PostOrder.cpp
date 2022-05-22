1d8761da128fdf6ee532f60728a990b4c4ed5d0e
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-22 17:44:16 +0530

    Construct BST from PostOrder

---
+++ Binary Tree Level 2/11.Construct_BST_PostOrder.cpp
@@ -0,0 +1,20 @@
+// Construct BST from Postorder
+
+
+Node *constructTree (int post[], int size)
+{
+      if(size<=0){
+       return NULL;
+   }
+   Node* root=new Node(post[size-1]);
+    
+   int i=0;
+   while(i<size-1 && post[size-1]>post[i]){
+       i++;
+   }
+   
+   root->left=constructTree(post+0,i);
+   root->right=constructTree(post+i,size-i-1);
+   
+   return root;
+}
