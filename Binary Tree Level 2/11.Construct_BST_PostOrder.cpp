3e913c0766af22f145c9a5b43eeacb6c78852fea
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-22 18:04:44 +0530

    Construct BST from PostOrder

--- Binary Tree Level 2/11.Construct_BST_PostOrder.cpp
+++ Binary Tree Level 2/11.Construct_BST_PostOrder.cpp
@@ -18,3 +18,32 @@ Node *constructTree (int post[], int size)
    
    return root;
 }
+
+// Approach 2
+
+
+class Solution {
+public:
+    
+    int idx = 0;
+    TreeNode* bstFromPostorder(vector<int> postorder, int lr, int rr) {
+        
+        if(idx >= postorder.size() or postorder[idx] < lr or postorder[idx] > rr){
+            return nullptr;
+        }
+        
+        TreeNode* root = new TreeNode(postorder[idx++]);
+        root->left = bstFromPostorder(postorder,lr,root->val);
+        root->right = bstFromPostorder(postorder,root->val,rr);
+        
+        return root;
+    }
+    
+    
+    TreeNode* bstFromPostorder(vector<int>& postorder) {
+        int n = postorder.size();
+        int lr = -1e9;
+        int rr = 1e9;
+        return bstFromPreorder(postorder,lr,rr);
+    }
+};
