d5112b0762a1ab0346e7d59a0c151bf6c86b507e
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-24 20:15:24 +0530

    Diameter of Bt

---
+++ Binary Tree Level 2/25.DiameterOfBT.cpp
@@ -0,0 +1,29 @@
+// 543. Diameter of Binary Tree
+
+class Solution {
+public:
+    
+    
+    int diameterOfBinaryTree_helper(TreeNode* root, int &dia) {
+        
+        if(root == nullptr){
+            return 0;
+        }
+        
+        int lht = diameterOfBinaryTree_helper(root->left,dia);
+        int rht = diameterOfBinaryTree_helper(root->right,dia);
+        
+       
+        
+        dia = max(lht+rht,dia);
+        int height = max(lht,rht) + 1;
+        
+        return height;
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root){
+        int dia = 0;
+        diameterOfBinaryTree_helper(root,dia);
+        return dia;
+    }
+};
