95f83fd35a8fe3732bbf10ae88eb263767d82761
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-25 22:50:34 +0530

    Pairs in Array whose sum is divisible by k

---
+++ Hashmaps/5.ArraySumDivByK.cpp
@@ -0,0 +1,46 @@
+// 1497. Check If Array Pairs Are Divisible by k
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        
+        unordered_map<int, int> m;
+        
+        for(int ele: arr){
+            int rem  = ele % k;
+            if(rem<0){
+                rem+=k;
+            }
+            m[rem]++;
+        }
+        
+        
+        for(int val: arr){
+            
+            int rem = val % k;
+            
+            if(rem == 0){
+                int freq = m[rem];
+                if(freq % 2 == 1){
+                    return false;
+                }
+            }
+            
+            else if(2*rem == 0){
+                int freq = m[rem];
+                if(freq % 2 == 1){
+                    return false;
+                }
+            }else{
+                int fq = m[rem];
+                int ofq = m[k-rem];
+                
+                if(fq != ofq){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
