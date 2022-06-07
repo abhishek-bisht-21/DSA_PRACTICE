62bb89e93f1bcd224c0ffd7725c7c849c4b069fc
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-06-07 12:26:55 +0530

    Heap Implementation

--- Heaps/0.HeapImplementation.cpp
+++ Heaps/0.HeapImplementation.cpp
@@ -79,17 +79,30 @@ void heapify(int arr[], int n, int i){
 	int left = 2 * i;
 	int right = 2 * i + 1;
 
-	if(left < n && arr[largest] < arr[left]){
+	if(left <= n && arr[largest] < arr[left]){
 		largest = left;
 	}
 
-	if(right < n && arr[largest] < arr[right]){
+	if(right <= n && arr[largest] < arr[right]){
 		largest = right;
 	}
 
 	if(largest != i){
 		swap(arr[largest], arr[i]);
 		heapify(arr,n,largest);
+	}
+}
+
+void heapSort(int arr[], int n){
+	int size = n;
+
+	while(size > 1){
+		// STEP1 : SWAP
+		swap(arr[size], arr[1]);
+		
+		size--;
+		// STEP 2
+		heapify(arr,size,1);
 	}
 }
 
@@ -126,6 +139,17 @@ int main(){
 	}
 
 	cout << endl;
+
+
+	//  HEAP SORT
+	heapSort(arr,n);
+		cout << "PRINT THE ARRAY NOW"<< endl;
+	for(int i=1;i<=n;i++){
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
 
 	return 0; 
 }
