8fc3981c02682a3e18ea7f37c51c9f1ce781e5ea
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-06-24 18:12:22 +0530

    Cycle Detection DFS

--- Graphs/11.CycleDetectionUndirected.cpp
+++ Graphs/11.CycleDetectionUndirected.cpp
@@ -60,3 +60,49 @@ string cycleDetection (vector<vector<int>>& edges, int n, int m)
 
 
 // APPROACH 2 : DFS
+
+bool isCyclicDFS(int src,int parent,unordered_map<int,bool> &visited,unordered_map<int, list<int>> &adj){
+    
+    visited[src] = true;
+    
+    for(auto nbr: adj[src]){
+        if(!visited[nbr]){
+            bool cycleDetected = isCyclicDFS(nbr,src,visited,adj);
+            if(cycleDetected)
+                return true;
+        }else if(nbr != parent){
+            // Cycle parent
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    // Write your code here.
+    // Creating Adjacency List
+    unordered_map<int, list<int>> adj;
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        
+        adj[u].push_back(v);
+        adj[v].push_back(u);      
+    }
+    
+    unordered_map<int,bool> visited;
+    
+    // To handle disconnected Component
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+            bool ans = isCyclicDFS(i,-1,visited,adj);
+            if(ans == 1){
+                return "Yes";
+            }
+        }
+    }
+    
+    return "No";
+}
