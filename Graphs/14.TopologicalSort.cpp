ee2366a97eaa16c8d9b370a4c01ab1394ef89cf0
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-06-27 03:00:05 +0530

    Topological Sort

--- Graphs/14.TopologicalSort.cpp
+++ Graphs/14.TopologicalSort.cpp
@@ -2,3 +2,48 @@
 //  u -> v , u appears before v	in that ordering.
 //  There can be multiple Topological Sort for a graph.
 // Topological Sort is applicable on Directed Acyclic Graphs.(DAG)
+
+// DFS APPROACH
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	
+	void findTopoSort(int node,vector<int> &vis, stack<int> &s,vector<int> adj[]){
+	    
+	    vis[node] = 1;
+	    for(auto it : adj[node]){
+	        if(!vis[it]){
+	            findTopoSort(it,vis,s,adj);
+	        }
+	    }
+	    
+	    s.push(node);
+	}
+	
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> vis(V,0);
+	    stack<int> s;
+	    
+	    for(int i=0;i<V;i++){
+	        if(vis[i] == 0){
+	            findTopoSort(i,vis,s,adj);
+	        }
+	    }
+	    
+	    vector<int> topoSort;
+	    while(!s.empty()){
+	        topoSort.push_back(s.top());
+	        s.pop();
+	    }
+	    
+	    return topoSort;
+	}
+};
+
+
+
+// BFS APPROACH
