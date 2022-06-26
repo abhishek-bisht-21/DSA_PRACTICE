916d691d9124321e8a531b4216960277495f497e
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-06-27 03:18:56 +0530

    BFS APPROACH -> Kahn's Algorithm

--- Graphs/14.TopologicalSort.cpp
+++ Graphs/14.TopologicalSort.cpp
@@ -46,4 +46,46 @@ class Solution
 
 
 
-// BFS APPROACH
+// BFS APPROACH -> Kahn's Algorithm
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    
+	    queue<int> q;
+	    vector<int> inDegree(V,0);
+	    
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]){
+	            inDegree[it]++;
+	        }
+	    }
+	    
+	    for(int i=0;i<V;i++){
+	        if(inDegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int> topo;
+	    
+	    while(!q.empty()){
+	        int frontNode = q.front();
+	        q.pop();
+	        topo.push_back(frontNode);
+	        
+	        for(auto it: adj[frontNode]){
+	            inDegree[it]--;
+	            if(inDegree[it] == 0){
+	                q.push(it);
+	            }
+	        }
+	    }
+	    
+	    return topo;
+	}
+};
