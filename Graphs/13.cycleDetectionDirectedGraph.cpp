2c634e5b28832f6817e438da1710c92324921e58
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-07-06 05:35:03 +0530

    Cycle Detection in Directed Graph DFS

--- Graphs/13.cycleDetectionDirectedGraph.cpp
+++ Graphs/13.cycleDetectionDirectedGraph.cpp
@@ -1,5 +1,5 @@
 
-
+// Approach 1
 class Solution{
 
 	public:
@@ -37,4 +37,57 @@ class Solution{
 	}
 
 	return false;
+}
+
+// Approach 2
+
+#include<bits/stdc++.h>
+bool checkCycle(int node, unordered_map<int,list<int>> &adj,
+               unordered_map<int,bool> &vis,unordered_map<int,bool> &dfsvis){
+    
+    vis[node] = true;
+    dfsvis[node] = true;
+    
+    for(auto nbr: adj[node]){
+        if(vis[nbr] && dfsvis[nbr]){
+            return true;
+        }
+        if(!vis[nbr]){
+            bool cycleDectected = checkCycle(nbr,adj,vis,dfsvis);
+            if(cycleDectected){
+                return true;
+            }
+        }
+    }
+    
+    dfsvis[node]  = false;
+    return false;
+}
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  // Write your code here.
+    
+    unordered_map<int,list<int>> adj;
+    // Create Adj List
+    for(int i=0;i<edges.size();i++){
+        
+        int u =  edges[i].first;
+        int v = edges[i].second;
+        
+        adj[u].push_back(v);
+    }
+    
+    unordered_map<int,bool> vis;
+    unordered_map<int,bool> dfsvis;
+    
+    
+    // Call Dfs for call components.
+    for(int i=1;i<=n;i++){
+        bool cycfound = checkCycle(i,adj,vis,dfsvis);
+        if(cycfound){
+            return true;
+        }
+    }
+    
+    return false;
 }
