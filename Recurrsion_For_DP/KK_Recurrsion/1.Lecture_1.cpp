7b2ec219eb2b12e2c1aa3e750106b11e1683ea22
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-08-09 22:36:18 +0530

    Recursion basics

--- Recurrsion_For_DP/KK_Recurrsion/1.Lecture_1.cpp
+++ Recurrsion_For_DP/KK_Recurrsion/1.Lecture_1.cpp
@@ -2,9 +2,30 @@
 using namespace std;
 
 
+int sumDigits(int num){
+	if(num % 10 == 0){
+		return 0;
+	}	
+	int smallAns = num % 10;
+	return smallAns + sumDigits(num/10);
+}
 
+int fact(int n){
+
+	if(n == 0){
+		return 1;
+	}
+
+	return n * fact(n-1);
+}
 
 int main(){
+
+	int n = 5;
+	int num = 3124;
+	cout << "Factorial of " << n << " is " << fact(n) << endl;
+	cout << "Sum of digits of " << num << " is " << sumDigits(num) << endl;
+
 
 	return 0;
 }
