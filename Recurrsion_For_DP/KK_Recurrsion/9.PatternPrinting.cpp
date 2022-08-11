ebaa07857c65800637e150e3858e60a0a4b4552f
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-08-11 11:47:38 +0530

    Printing Patterns

--- Recurrsion_For_DP/KK_Recurrsion/9.PatternPrinting.cpp
+++ Recurrsion_For_DP/KK_Recurrsion/9.PatternPrinting.cpp
@@ -16,7 +16,29 @@ void patternPrinting(int n){
 }
 
 
+void patternPrinting2(int n){
+	if(n == 0){
+		return;
+	}
+
+	for(int i=1;i<=n;i++){
+		cout << i << " ";
+	}
+
+	cout <<"\n";
+
+	patternPrinting2(n-1); // TRUST the function
+
+	for(int i=1;i<=n;i++){
+		cout << i << " ";
+	}
+
+	cout <<"\n";
+}
+
+
+
 int main(){
-	patternPrinting(5);
+	patternPrinting2(4);
 	return 1;
 }
