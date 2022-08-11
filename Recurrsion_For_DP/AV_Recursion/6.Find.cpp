9ba1cef6a9e9fd93940c16ac7949fb4645f5f89f
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-08-11 20:50:40 +0530

    Find in the Array

---
+++ Recurrsion_For_DP/AV_Recursion/6.Find.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+bool find(vector<int> &arr,int key, int idx){
+
+	if(idx == arr.size()){
+		return false;
+	}
+
+	return (arr[idx] == key || find(arr,key,idx+1));
+}
+
+int main(){
+
+	int key = 10;
+	vector<int> v{10,20,30,40,50};
+	cout << "Is Element " << key<< " present in the array " << boolalpha << find(v,key,0);
+
+	return 1;
+}
