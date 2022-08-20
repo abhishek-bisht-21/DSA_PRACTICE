1d5f247ad65659567ea048c2905c85423d556be7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-08-20 17:16:05 +0530

    Array Sum

--- Recurrsion_For_DP/LB_Recursion/1.Lecture.cpp
+++ Recurrsion_For_DP/LB_Recursion/1.Lecture.cpp
@@ -13,9 +13,24 @@ bool isArraySorted(vector<int> arr, int idx){
 }
 
 
+// Get Array Sum
+
+int arraySum(vector<int> &arr, int idx){
+	if(idx == arr.size()){
+		return 0;
+	}
+
+	int small_ans = arraySum(arr,idx+1);
+	int myans = small_ans + arr[idx];
+	return myans;
+}
+
+
 int main(){
 
 	vector<int> v{1,2,3,4,5,0};
 	int idx = 0;
-	cout << "Is Array Sorted " << boolalpha << isArraySorted(v,idx);
+	cout << "Is Array Sorted " << boolalpha << isArraySorted(v,idx) << endl;
+	cout << "Array Sum " << arraySum(v,idx) << endl;
+
 }
