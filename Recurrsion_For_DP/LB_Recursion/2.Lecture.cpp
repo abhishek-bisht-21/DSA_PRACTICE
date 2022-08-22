a4942555a6270841def189744da6dececae7c4d7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-08-22 21:56:17 +0530

    Check Palindorme

--- Recurrsion_For_DP/LB_Recursion/2.Lecture.cpp
+++ Recurrsion_For_DP/LB_Recursion/2.Lecture.cpp
@@ -15,6 +15,24 @@ void reverseString(string &str, int s, int e){
 }
 
 
+// Check Palindrome
+bool checkPalindrome(string str, int s, int e){
+	
+	if(s == e){
+		return true;
+	}
+
+	if(str[s] != str[e]){
+		return false;
+	}
+
+	bool res = checkPalindrome(str,s+1,e-1);
+
+	return res; 
+
+}
+
+
 int main(){
 
 	string str = "DOG";
@@ -22,6 +40,9 @@ int main(){
 	cout << "Before Reversing the string: " << str << endl;
 	reverseString(str,0,len-1);
 	cout << "After Reversing the string: " << str << endl;
+
+	string s = "naman";
+	cout << "Check Palindome : "<< boolalpha << checkPalindrome(s,0,s.length()-1) << endl;
 
 
 
