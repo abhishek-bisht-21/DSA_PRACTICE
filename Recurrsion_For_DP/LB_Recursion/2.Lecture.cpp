1a6354d1bd5358558665b898146574158add8689
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-08-22 22:36:52 +0530

    Power of a number

--- Recurrsion_For_DP/LB_Recursion/2.Lecture.cpp
+++ Recurrsion_For_DP/LB_Recursion/2.Lecture.cpp
@@ -32,17 +32,51 @@ bool checkPalindrome(string str, int s, int e){
 
 }
 
+// Calculating Power
+
+int power(int a, int b){
+	
+	if(b == 0){
+		return 1;
+	}
+
+	int small_ans = power(a,b-1);
+	int myans = a * small_ans;
+	return myans;
+}
+
+
+
+int powerOptimised(int a, int b){
+	
+	if(b == 0){
+		return 1;
+	}
+
+	int small_ans = powerOptimised(a,b/2);
+	int myans = small_ans * small_ans;
+	if(b % 2 != 0){
+		myans = a *  myans;
+	}
+	return myans;
+}
 
 int main(){
 
-	string str = "DOG";
-	int len = str.length();
-	cout << "Before Reversing the string: " << str << endl;
-	reverseString(str,0,len-1);
-	cout << "After Reversing the string: " << str << endl;
+	// string str = "DOG";
+	// int len = str.length();
+	// cout << "Before Reversing the string: " << str << endl;
+	// reverseString(str,0,len-1);
+	// cout << "After Reversing the string: " << str << endl;
 
-	string s = "naman";
-	cout << "Check Palindome : "<< boolalpha << checkPalindrome(s,0,s.length()-1) << endl;
+	// string s = "naman";
+	// cout << "Check Palindome : "<< boolalpha << checkPalindrome(s,0,s.length()-1) << endl;
+
+	int a = 2;
+	int b = 5;
+	cout << "Calculate " << a << "^"<< b << " = " << power(a,b) << endl;
+	cout << "Calculate " << a << "^"<< b << " = " << powerOptimised(a,b) << endl;
+
 
 
 
