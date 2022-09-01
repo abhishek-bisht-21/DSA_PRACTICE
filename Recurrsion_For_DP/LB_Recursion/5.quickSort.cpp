3bcb9913e2eb676b7deb6c90598c8b1592ef3714
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-01 17:53:29 +0530

    QuickSort

--- Recurrsion_For_DP/LB_Recursion/5.quickSort.cpp
+++ Recurrsion_For_DP/LB_Recursion/5.quickSort.cpp
@@ -15,7 +15,7 @@ void quickSort(int *arr, int low, int high)
         quickSort(arr,partitionIdx+1,high);
     }
     
-    
+  
     int partition (int *arr, int low, int high)
     {
        // Your code here
@@ -58,4 +58,70 @@ int main(){
 
 
 	return 1;
+}
+
+
+
+// ALTERNATE APPROACH
+
+int partition( vector<int> &arr, int s, int e) {
+
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for(int i = s+1; i<=e; i++) {
+        if(arr[i] <=pivot) {
+            cnt++;
+        }
+    }
+
+    //place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    //left and right wala part smbhal lete h 
+    int i = s, j = e;
+
+    while(i < pivotIndex && j > pivotIndex) {
+
+        while(arr[i] <= pivot) 
+        {
+            i++;
+        }
+
+        while(arr[j] > pivot) {
+            j--;
+        }
+
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+
+    }
+
+    return pivotIndex;
+
+}   
+
+void solve(vector<int> &arr, int s, int e) {
+
+    //base case
+    if(s >= e) 
+        return ;
+
+    //partitioon karenge
+    int p = partition(arr, s, e);
+
+    //left part sort karo
+    solve(arr, s, p-1);
+
+    //right wala part sort karo
+    solve(arr, p+1, e);
+
+}
+vector<int> quickSort(vector<int> arr)
+{
+    // Write your code here.
+    solve(arr,0,arr.size()-1);
+    return arr;
 }
