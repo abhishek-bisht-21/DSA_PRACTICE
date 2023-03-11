418a9da64da56145bbe2356f47c5d18798216490
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-11 12:49:39 +0530

    Kth Largest Element in Unsorted Array

---
+++ A2Z DSA Course/HashMaps_&_Heaps/Problems On Heap/2.Kth_Largest.cpp
@@ -0,0 +1,18 @@
+// 215. Kth Largest Element in an Array
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> minh;
+        int n = nums.size();
+
+        for(int i=0;i<n;i++){
+            minh.push(nums[i]);
+            if(minh.size() > k){
+                minh.pop();
+            }
+        }
+
+        return minh.top();
+    }
+};
