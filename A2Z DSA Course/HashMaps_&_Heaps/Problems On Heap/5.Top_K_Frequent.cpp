6dfffaf0d11a365cbfb080df7939f71bd07c2b14
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-12 08:55:25 +0530

    Sort Array by Increasing Frequency

---
+++ A2Z DSA Course/HashMaps_&_Heaps/Problems On Heap/5.Top_K_Frequent.cpp
@@ -0,0 +1,25 @@
+// 347. Top K Frequent Elements
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans;
+        unordered_map<int,int> m;
+        
+        for(int val:nums){
+            m[val]++;
+        }
+        
+        priority_queue<pair<int,int>> pq;
+        for(auto p: m){
+            pq.push({p.second,p.first});
+        }
+        
+        while(k-- > 0){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
