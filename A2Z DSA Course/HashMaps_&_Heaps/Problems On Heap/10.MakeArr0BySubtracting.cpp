036738445b115b72e8f6633a6c7e0da2d9ba2ab7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-14 23:49:02 +0530

    2357. Make Array Zero by Subtracting Equal Amounts

---
+++ A2Z DSA Course/HashMaps_&_Heaps/Problems On Heap/10.MakeArr0BySubtracting.cpp
@@ -0,0 +1,21 @@
+// 2357. Make Array Zero by Subtracting Equal Amounts
+class Solution {
+public:
+int minimumOperations(vector<int>& nums) {
+    priority_queue<int , vector<int> , greater<int>> minH(nums.begin() , nums.end());
+    int curr_min = 0;
+    int ans = 0;
+    while(!minH.empty()){
+        if(minH.top() == 0) minH.pop();
+        else{
+            int tp = minH.top() - curr_min;
+            if(tp != 0){
+                curr_min += tp;
+                ans++;
+            }
+            minH.pop();
+        }
+    }
+    return ans;
+}
+};
