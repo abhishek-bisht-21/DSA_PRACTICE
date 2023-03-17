66cb0c17e0aac136705fa8bfde3b78237f1f4033
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-18 02:14:47 +0530

    Merge K Sorted Arrays

--- A2Z DSA Course/HashMaps_&_Heaps/Problems On Heap/15.MergeKSortedArr.cpp
+++ A2Z DSA Course/HashMaps_&_Heaps/Problems On Heap/15.MergeKSortedArr.cpp
@@ -29,3 +29,65 @@ class Solution
 
 
 // Approach 2: Optimised TC-> (n*k)logk
+
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    #include <bits/stdc++.h> 
+
+
+class node{
+    public:
+    int data;
+    int i;
+    int j;
+
+    node(int data,int row, int col){
+        this->data = data;
+        this->i = row;
+        this->j = col;
+    }
+};
+
+
+class compare{
+    public:
+
+    bool operator()(node* a, node* b){
+        return a->data > b->data;
+    }
+};
+
+vector<int> mergeKArrays(vector<vector<int>> kArrays, int k)
+{
+    priority_queue<node*, vector<node*>, compare> pq;
+
+    // Step1: Insert all the 1st Element of All the Arrays
+    for(int i=0;i<k;i++){
+        node* tmp = new node(kArrays[i][0], i, 0);
+        pq.push(tmp);
+    }
+
+    vector<int> ans;
+
+    //Step2: 
+    while(!pq.empty()){
+
+        node* topVal = pq.top();
+        ans.push_back(topVal->data);
+        pq.pop();
+
+        int row = topVal->i;
+        int col = topVal->j;
+
+        if(col+1 < kArrays[row].size()){
+            node* next = new node(kArrays[row][col+1],row,col+1);
+            pq.push(next);
+        }
+    }
+
+    return ans;
+}
+
+};
