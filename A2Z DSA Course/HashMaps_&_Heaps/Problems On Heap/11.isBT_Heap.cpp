32550130740c961a4e351b8725887ee23cd56f77
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-17 05:41:33 +0530

    is BT a Heap

--- A2Z DSA Course/HashMaps_&_Heaps/Problems On Heap/11.isBT_Heap.cpp
+++ A2Z DSA Course/HashMaps_&_Heaps/Problems On Heap/11.isBT_Heap.cpp
@@ -5,13 +5,9 @@ class Solution {
   int countNodes(struct Node* root){
 
       if(root == NULL)
-
       return 0;
 
-      
-
       int ans = 1 + countNodes(root->left) + countNodes(root->right);
-
       return ans;
 
   }
@@ -19,47 +15,39 @@ class Solution {
   bool isCBT(struct Node* root, int i, int cnt){
 
       if(root == NULL){
-
           return true;
-
       }
 
-      
 
       if(i >= cnt){
-
           return false;
-
       }
 
       else{
-
           bool left = isCBT(root->left, 2*i+1, cnt);
-
           bool right = isCBT(root->right, 2*i+2, cnt);
-
-          
-
           return (left && right);
-
       }
 
   }
 
   bool isMaxOrder(struct Node* root){
 
+      // Is Leaf Node. Cause Leaf Node is Always a Heap.Always follow heapOrderProp	  
       if(root->left == NULL && root->right == NULL){
 
           return true;
 
       }
 
+      // Only Having Left Child	
       if(root->right == NULL){
 
           return root->data > root->left->data;
 
       }
 
+      // Having Both the child Nodes	
       else{
 
           bool left = isMaxOrder(root->left);
@@ -81,7 +69,8 @@ class Solution {
         int countNode = countNodes(tree);
 
         
-
+	// 2 Necessary Condition for BT to be a Heap.
+	// Complete Binary Tree and HeapOrderProperty => True
         if(isCBT(tree, index, countNode) && isMaxOrder(tree)){
 
             return true;
