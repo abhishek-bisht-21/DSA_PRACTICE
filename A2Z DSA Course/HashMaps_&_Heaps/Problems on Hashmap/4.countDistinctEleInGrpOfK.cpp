2d11415896d8f64a8d1bafd3229715a5ca2ff62b
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-19 10:54:35 +0530

    4.countDistinctEleInGrpOfK.cpp

--- A2Z DSA Course/HashMaps_&_Heaps/Problems on Hashmap/4.countDistinctEleInGrpOfK.cpp
+++ A2Z DSA Course/HashMaps_&_Heaps/Problems on Hashmap/4.countDistinctEleInGrpOfK.cpp
@@ -1,31 +1,34 @@
 // Acquire and Release
 // Count distinct elements in every window
+
 class Solution{
   public:
     vector <int> countDistinct (int arr[], int n, int k)
     {
         //code here.
+        unordered_map<int, int> umap;
         
-        unordered_map<int,int> m;
-        
+        // 1st Acquire ele till k 
         for(int i=0;i<k;i++){
-            m[arr[i]]++;
+            umap[arr[i]]++;
         }
         
         vector<int> res;
-        res.push_back(m.size());
+        res.push_back(umap.size());
         
         for(int i=k;i<n;i++){
             
-            m[arr[i-k]]--;
+            // Release
+            umap[arr[i-k]]--;
             
-            if(m[arr[i-k]] == 0){
-                m.erase(arr[i-k]);
+            if(umap[arr[i-k]] == 0){
+                umap.erase(arr[i-k]);    
             }
             
-            m[arr[i]]++;
+            // Acquire
+            umap[arr[i]]++;
             
-            res.push_back(m.size());
+            res.push_back(umap.size());
         }
         
         return res;
