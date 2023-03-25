4d226a4f0b881c95a9a5363295613cd23c0a16fa
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-25 11:15:23 +0530

    Added Initial Code For Heap Sort

---
+++ A2Z DSA Course/HashMaps_&_Heaps/heapSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+
+void heapSort(vector<int> &arr){
+
+}
+
+void heapSort(vector<int> &arr, bool isIncreasing){
+
+}
+
+int main(){
+
+	vector<int> arr = {10,20,30,-2,-3,-4,5,6,7,8,9,22,11,13};
+	heapSort(arr);
+	// inc/dec sorted
+	heapSort(arr,true);
+
+	return 0;
+}
