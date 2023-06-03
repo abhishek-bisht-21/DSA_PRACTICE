79a352332340de511513035ea2d69fd481ed6154
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-06-03 15:38:42 +0530

    Insert At Bottom in Stack

---
+++ Stacks/5.InsertAtBottomOrAnyIdx.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+
+void insertAtBottom(stack<int> &st, int x){
+
+	stack<int> temp;
+	while(not st.empty()){
+		int curr = st.top();
+		st.pop();
+		temp.push(curr);
+	}
+
+	st.push(x); // x got inserted at bottom
+
+	while(not temp.empty()){
+		int curr = temp.top();
+		temp.pop();
+		st.push(curr);
+	}
+}
+
+void printStack(stack<int> st){
+
+	while(!st.empty()){
+		cout << st.top() << endl;
+		st.pop();
+	}
+}
+
+int main(){
+
+	stack<int> st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.push(4);
+
+	cout << "Stack Before Inserting at bottom: "<< endl;
+	printStack(st);
+	insertAtBottom(st,10);
+	cout << "Stack after inserting at bottom: " << endl;
+	printStack(st);
+
+	return 0;
+}
