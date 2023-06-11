13b78ee04cc8845aaffc3024cb82bfe19cc56424
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-06-11 11:13:13 +0530

    Next Greater Element

---
+++ Stacks/11.nextGteaterElement.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> nge(vector<int> &arr){
+	int n = arr.size();
+	vector<int> output(n,-1);
+	stack<int> st;
+	st.push(0);
+
+	for(int i=1;i<n;i++){
+		while(!st.empty() and arr[i] > arr[st.top()]){
+			output[st.top()] = arr[i];
+			st.pop();			
+		}
+
+		st.push(i);
+	}
+
+	while(not st.empty()){ // not mandatory while loop
+		output[st.top()] = -1;
+		st.pop();
+	}
+
+	return output;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	vector<int> v;
+
+	while(n--){
+		int x;
+		cin>> x;
+		v.push_back(x);
+	}
+
+	vector<int> res = nge(v);
+	for(int ele:res){
+		cout << ele << endl;
+	}
+	return 0;
+}
