d5e80b288925348e884d7fe5e7377ad8496a9635
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-07-15 12:33:46 +0530

    Reverse a Linkedlist Iterative Method

--- LinkedList/5.reverse_LL.cpp
+++ LinkedList/5.reverse_LL.cpp
@@ -29,18 +29,18 @@ public:
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        
-        ListNode* prev = NULL;
         ListNode* curr = head;
-        ListNode* next_next = nullptr;
-        
-        while(curr){
+        ListNode* next_next;
+        ListNode* prev = NULL;
+
+
+        while(curr != NULL){
             next_next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next_next;
         }
-        
+
         return prev;
     }
 };
