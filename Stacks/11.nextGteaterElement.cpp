ca1e4cade28a21ce4bef92b64547a4f406ee0e36
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-07-21 00:38:40 +0530

    Explanation added

--- Stacks/11.nextGteaterElement.cpp
+++ Stacks/11.nextGteaterElement.cpp
@@ -1,3 +1,17 @@
+/*
+It is always easy to find the nge for increasing curve cause the answer is the next element only,
+But for decreasing curve we have to wait till we find the increasing curve again, We have to
+wait and park the elements which we encountered in the decreasing curve somewhere.
+
+Whenever such a suitation arise when we have to wait for a elements, then stack is the best
+option.	After every decreasing curve when we get a increasing curve then we will surely have 
+answer for the last element that we encounter and we can get the last element with help of stack.
+
+In the stack we will store then index of the elements, cause using index we can get the value of
+element but vice-versa is not true. Another reason of doing so is that we have to maintain a
+output array, by storing index we would know at what index in output array what value we have to store.
+*/
+
 #include <iostream>
 #include <stack>
 #include <vector>
