45775a456bbccb7908d4fb14cd8d8ef483a21e09
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-08-04 10:23:31 +0530

    Left View of BT recursive approach

--- Binary Tree Level 2/15.Left_View_BT.cpp
+++ Binary Tree Level 2/15.Left_View_BT.cpp
@@ -31,3 +31,30 @@ vector<int> leftView(Node *root)
    
    return ans;
 }
+
+
+// Approach 2 Preorder Traversal
+
+void leftViewHelper(Node* root, int level, vector<int> &ans){
+    if(!root){
+        return;
+    }
+    
+    // First time we are encountering a level.
+    // It ensures that we are storing the first node from left.
+    if(level == ans.size()){
+        ans.push_back(root->data);
+    }
+    
+    leftViewHelper(root->left,level+1,ans);
+    leftViewHelper(root->right,level+1,ans);
+}
+
+vector<int> leftView(Node *root)
+{
+   // Your code here
+   int level=0;
+   vector<int> ans;
+   leftViewHelper(root,level,ans);
+   return ans;
+}
