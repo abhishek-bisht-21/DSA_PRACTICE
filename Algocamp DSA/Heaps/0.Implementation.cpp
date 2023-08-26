2ed686a9cb0a1e6d3652288a1b0984a234a6e930
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-08-27 05:27:15 +0530

    Down Heapify Algorithm implemented

--- Algocamp DSA/Heaps/0.Implementation.cpp
+++ Algocamp DSA/Heaps/0.Implementation.cpp
@@ -116,5 +116,18 @@ int main(){
 	h.deleteFromHeap();
 	h.print();
 
+	int arr[6] = {-1, 54,53,55,52,50};
+	int n = 5;
+	for(int i=n/2;i>0;i--){
+		h.heapify(arr,n,i);
+	}
+
+	cout << "printing array elements: "<< endl;
+
+	for(int i=1;i<=n;i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
