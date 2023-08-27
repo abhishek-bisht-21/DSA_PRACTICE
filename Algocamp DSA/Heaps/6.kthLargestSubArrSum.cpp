c8c14e43167ac90620522854f7faafdec2b90587
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-08-27 10:22:42 +0530

    K-th Largest Sum Contiguous Subarray

---
+++ Algocamp DSA/Heaps/6.kthLargestSubArrSum.cpp
@@ -0,0 +1,28 @@
+//User function Template for C++
+
+class Solution{
+public:
+    int kthLargest(vector<int> &Arr,int N,int K){
+        
+        vector<int> sumArr;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int i=0;i<N;i++){
+            int sum = 0;
+            for(int j=i;j<N;j++){
+                sum = sum+Arr[j];
+            
+            
+            pq.push(sum);
+            
+            if(pq.size()>K){
+                pq.pop();
+            }
+        }
+
+    }
+        return pq.top();
+    }
+    
+
+};
