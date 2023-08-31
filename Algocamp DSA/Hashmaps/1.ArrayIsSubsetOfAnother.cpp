e4630aabc07e842a2a90bb3f1da2015860922964
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-08-31 09:26:16 +0530

    string isSubset(int a1[], int a2[], int n, int m) {

---
+++ Algocamp DSA/Hashmaps/1.ArrayIsSubsetOfAnother.cpp
@@ -0,0 +1,13 @@
+// Array Subset of another array
+
+string isSubset(int a1[], int a2[], int n, int m) {
+    
+   unordered_map<int,int> um;
+    for(int i=0;i<n;i++)um[a1[i]]++;
+    for(int i=0;i<m;i++)
+    {
+        if(um[a2[i]]>0)um[a2[i]]--;
+        else return "No";
+    }
+    return "Yes";
+}
