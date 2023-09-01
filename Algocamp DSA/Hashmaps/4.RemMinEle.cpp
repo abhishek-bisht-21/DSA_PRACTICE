7017ccbcf23cbe202625ae55b3a89719ba73f220
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-09-01 10:05:26 +0530

    Remove minimum number of elements

--- Algocamp DSA/Hashmaps/4.RemMinEle.cpp
+++ Algocamp DSA/Hashmaps/4.RemMinEle.cpp
@@ -6,19 +6,21 @@ class Solution{
     int minRemove(int a[], int b[], int n, int m) {
         int elementsRemoved = 0;
         
-        unordered_map<int, int> amap;
+        unordered_map<int, int> amap; // Element, Freq
         for(int i=0;i<n;i++){
             amap[a[i]]++;
         }
         
-        unordered_map<int, int> bmap;
+        unordered_map<int, int> bmap; // Element, Freq
         for(int i=0;i<m;i++){
             bmap[b[i]]++;
         }
         
 
         for(auto x: amap){
+		// Finding Common Elements in Array A and B
             if(bmap.find(x.first) != bmap.end()){
+		    // Which ever element is common and has less freq will be removed.
                 int freq_to_rem = x.second > bmap[x.first] ? bmap[x.first] : x.second;
                 elementsRemoved+=freq_to_rem;
             }
