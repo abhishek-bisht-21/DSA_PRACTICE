8f3290629fbddb97b2a77060506a90580d13b560
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-09-09 12:47:47 +0530

    Count distinct elements in every window

---
+++ Algocamp DSA/Hashmaps/9.CountDistinctEleInWindowOFSizeK.cpp
@@ -0,0 +1,35 @@
+
+// Count distinct elements in every window
+class Solution{
+  public:
+    vector <int> countDistinct (int arr[], int n, int k)
+    {
+        //code here.
+       unordered_map<int, int> umap;
+        
+        // 1st Acquire ele till k 
+        for(int i=0;i<k;i++){
+            umap[arr[i]]++;
+        }
+        
+        vector<int> res;
+        res.push_back(umap.size());
+        
+        for(int i=k;i<n;i++){
+            
+            // Release
+            umap[arr[i-k]]--;
+            
+            if(umap[arr[i-k]] == 0){
+                umap.erase(arr[i-k]);    
+            }
+            
+            // Acquire
+            umap[arr[i]]++;
+            
+            res.push_back(umap.size());// Storing the unique elements in the new window
+        }
+        
+        return res;
+    }
+};
