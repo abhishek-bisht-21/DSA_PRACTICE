2ffffa805fa1f0ffa5e76a035bc9bbb03bbd411f
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-11-13 12:25:54 +0530

    875. Koko Eating Bananas

---
+++ Binary Search/28.KokoEatingBananas.cpp
@@ -0,0 +1,40 @@
+
+// 875. Koko Eating Bananas
+
+/*
+
+We know the range in which our answer would lie
+
+*/
+// Appraoch 1
+class Solution {
+public:
+    
+    
+     bool isfeasible(vector<int>& piles, int h,int mid)
+    {
+        double ans = 0;
+        for(auto p : piles)
+            ans += ceil(p*1.0/mid);
+        
+        return ans <= h;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int minSpeed = 1; // Low
+        int maxSpeed = *max_element(piles.begin(),piles.end()); // High
+        int ans = -1;
+        while(minSpeed <= maxSpeed){
+            int mid = minSpeed + (maxSpeed - minSpeed)/2;
+            if(isfeasible(piles,h,mid)){
+                ans = mid;
+                maxSpeed = mid - 1; // We got our answer but we are trying to find the minimum possible hours in which koko can eat entire piles
+            }
+            else{
+                minSpeed = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
