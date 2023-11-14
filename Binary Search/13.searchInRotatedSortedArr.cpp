046bdd052986cb18e9dfddaa4dcf3519aea80768
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-11-15 02:32:24 +0530

    Search In Rotated Sorted Array

--- Binary Search/13.searchInRotatedSortedArr.cpp
+++ Binary Search/13.searchInRotatedSortedArr.cpp
@@ -3,6 +3,10 @@
 /*
 	Every Time either the left part or right the part will be sorted.
 	We can determine where our given element is will exist, either in rp or in lp. 
+
+    Our element would lie in 1 of the 2 sorted parts (either Left part or right).
+    If we somehow reduce our search space such that we find the half in which our target element is present.
+    Then it is a simple Binary Search.
 */
 
 
