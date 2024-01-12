e1bbe1e9a9cc0142d9aa775d128482497a13355b
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-01-13 00:35:01 +0530

    1704. Determine if String Halves Are Alike

---
+++ Jan 2024 Leetcode Daily Challenge/12.Determine if String Halves Are Alike.cpp
@@ -0,0 +1,22 @@
+
+// 1704. Determine if String Halves Are Alike
+
+#include <bits/stdc++.h>
+class Solution {
+public:
+    bool halvesAreAlike(std::string s) {
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int vowelsCount = 0;
+        int midIndex = s.length() / 2;
+
+        for (int i = 0; i < midIndex; i++) {
+            char charA = s[i];
+            char charB = s[midIndex + i];
+            if (vowels.count(charA)) vowelsCount++;
+            if (vowels.count(charB)) vowelsCount--;
+        }
+
+        return vowelsCount == 0;
+    }
+};
+
