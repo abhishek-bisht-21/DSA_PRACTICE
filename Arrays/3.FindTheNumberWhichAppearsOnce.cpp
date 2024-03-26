42bf6c30f8ee8c567058242a5d48114afe806005
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-03-27 01:58:08 +0530

    Find the number that appears once, and the other numbers twice

---
+++ Arrays/3.FindTheNumberWhichAppearsOnce.cpp
@@ -0,0 +1,113 @@
+
+
+
+
+// Approach 1: Most Optimal XOR
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int> &arr) {
+    //size of the array:
+    int n = arr.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
+
+
+// Using Maps
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int> &arr) {
+
+    //size of the array:
+    int n = arr.size();
+
+    // Declare the hashmap.
+    // And hash the given array:
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        mpp[arr[i]]++;
+    }
+
+    //Find the single element and return the answer:
+    for (auto it : mpp) {
+        if (it.second == 1)
+            return it.first;
+    }
+
+    //This line will never execute
+    //if the array contains a single element.
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
+
+
+//  Using Hash Array
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int> &arr) {
+
+    //size of the array:
+    int n = arr.size();
+
+    // Find the maximum element:
+    int maxi = arr[0];
+    for (int i = 0; i < n; i++) {
+        maxi = max(maxi, arr[i]);
+    }
+
+    // Declare hash array of size maxi+1
+    // And hash the given array:
+    vector<int> hash(maxi + 1, 0);
+    for (int i = 0; i < n; i++) {
+        hash[arr[i]]++;
+    }
+
+    //Find the single element and return the answer:
+    for (int i = 0; i < n; i++) {
+        if (hash[arr[i]] == 1)
+            return arr[i];
+    }
+
+    //This line will never execute
+    //if the array contains a single element.
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
