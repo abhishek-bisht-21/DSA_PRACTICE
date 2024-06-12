57785fd023e90384b00662e4fe10a57e724a24e7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-12 22:46:20 +0530

    Print All Subsequences of a string

---
+++ A2Z DSA Course/Recursion/9.PrintAllSubSeq.cpp
@@ -0,0 +1,41 @@
+// https://www.geeksforgeeks.org/problems/power-set4302/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+	
+	    void solve(string& s,string temp,vector<string>&ans,int index){
+            if(index==s.length()){
+                if(temp.length()>=1)
+                ans.push_back(temp);
+                return;
+            }
+            solve(s,temp,ans,index+1);
+            solve(s,temp+s[index],ans,index+1);
+        }
+        vector<string> AllPossibleStrings(string s){
+            vector<string>ans;
+            solve(s,"",ans,0);
+            sort(ans.begin(),ans.end());
+            return ans;
+            
+        }
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
